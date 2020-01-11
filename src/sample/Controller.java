ad02cc76bc0bcf65a60dbae11cb8db399629bc22
DenisKantic <denis.kantic18@gmail.com>
2020-01-11 21:59:23 +0100

    small fix in style.css and added methods on buttons for printing numbers on display label

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,43 @@
 package sample;
 
+import javafx.scene.control.Label;
+
 public class Controller {
 
+        public Label mainLabel;
+        public Label history;
+
+
+        public void zero(){
+            mainLabel.setText(mainLabel.getText()+"0");
+        }
+
+        public void one(){
+            mainLabel.setText(mainLabel.getText()+"1");
+        }
+    public void two(){
+        mainLabel.setText(mainLabel.getText()+"2");
+    }
+    public void three(){
+        mainLabel.setText(mainLabel.getText()+"3");
+    }
+    public void four(){
+        mainLabel.setText(mainLabel.getText()+"4");
+    }
+    public void five(){
+        mainLabel.setText(mainLabel.getText()+"5");
+    }
+    public void six(){
+        mainLabel.setText(mainLabel.getText()+"6");
+    }
+    public void seven(){
+        mainLabel.setText(mainLabel.getText()+"7");
+    }
+    public void eight(){
+        mainLabel.setText(mainLabel.getText()+"8");
+    }
+    public void nine(){
+        mainLabel.setText(mainLabel.getText()+"9");
+    }
 
 }
